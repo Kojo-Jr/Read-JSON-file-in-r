f2066f0b3e604bf110f1e3750e474f7d87909bbc
kojo-jr <omarieben@outlook.com>
2024-08-07 20:34:07 +0000

    update

--- 01_r_script/Read_JSON_file_in_r.R
+++ 01_r_script/Read_JSON_file_in_r.R
@@ -14,3 +14,13 @@ print(JsonData)
 # convert to a data frame
 jsonData_to_Dataframe <- as.data.frame(JsonData)
 View(jsonData_to_Dataframe)
+
+
+# You can also read it this way
+jsonData1 <- read_json("./00_data/example_1.json")
+jsonData_to_Dataframe1 <- data.frame(jsonData1)
+View(jsonData_to_Dataframe1)
+
+jsonData2 <- read_json("./00_data/sample2.json")
+jsonData_to_Dataframe2 <- data.frame(jsonData2)
+View(jsonData_to_Dataframe2)
